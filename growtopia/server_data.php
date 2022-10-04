7dd2d6d199775913483c686a429b80988054e9ae
badewen <81739844+badewen@users.noreply.github.com>
2022-10-04 21:25:23 +0700

    Add files via upload

---
+++ growtopia/server_data.php
@@ -0,0 +1,16 @@
+server|127.0.0.1
+port|17199
+type|1
+#maint|Server is under maintenance. We will be back online shortly. Thank you for your patience!
+beta_server|beta.growtopiagame.com
+beta_port|26999
+beta_type|1
+beta2_server|beta2.growtopiagame.com
+beta2_port|26999
+beta2_type|1
+beta3_server|beta-pub.growtopiagame.com
+beta3_port|26999
+beta3_type|1
+type2|1
+meta|4b4Pf1ZirgPGHnLenX2SFcWlTPG0No+W1TnNO+U3cjo=
+RTENDMARKERBS1001
